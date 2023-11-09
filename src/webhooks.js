4b9c17c7fbe47572983d63f71965b39880140775
gabrielpasini <gabrielpasini@outlook.com.br>
2023-11-09 15:41:59 -0300

    :pencil: transforma urls dos webhooks em variaveis de ambiente

--- src/webhooks.js
+++ src/webhooks.js
@@ -1,24 +1,17 @@
 const axios = require('axios');
 
 async function authWebhook(content) {
-  return await axios.post(
-    'https://discord.com/api/webhooks/1066973454936588328/rT87xaG195goHzJ62ae_Qk_VreJEjD92UhNqTYy3Zrm3TZmP5cCf38rxVLyXulaPXMKe',
-    { content }
-  );
+  return await axios.post(process.env.WEBHOOK_AUTH, { content });
 }
 
 async function errorLog(content) {
-  return await axios.post(
-    'https://discord.com/api/webhooks/1066977872914415627/8oGRFuEsW6kwLZBljDKSMN7MoA526cKAEC6rY1DJ1eegsgzBFCnHxd8Za8JLOJOX0pzl',
-    { content: '```' + content + '```' }
-  );
+  return await axios.post(process.env.WEBHOOK_ERROR, {
+    content: '```' + content + '```',
+  });
 }
 
 async function successMessage(content) {
-  return await axios.post(
-    'https://discord.com/api/webhooks/1168643651405090817/2ryYvjlilIsfYtRsfFvePZeQ-pR-LtnUYDmyfmTB6gCcdpxwvk4iorBSEsNQq5_BKcQf',
-    { content }
-  );
+  return await axios.post(process.env.WEBHOOK_SUCCESS, { content });
 }
 
 module.exports = { authWebhook, errorLog, successMessage };
